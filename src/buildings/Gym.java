fc042c0569da0128e4849c16b2930c4d06f6a5b9
Blokitin Oleh <blokitin.oleg.fca@gmail.com>
2022-06-23 08:30:09 +0200

    added variables

--- src/buildings/Gym.java
+++ src/buildings/Gym.java
@@ -1,8 +1,14 @@
 package buildings;
 
 public class Gym {
+
     private String name;
     private String address;
+    private String owner;
     private double area;
+    private double monthlyRent;
+    private double openingTime;
+    private double closingTime;
+    private int numberOfRooms;
 
 }
